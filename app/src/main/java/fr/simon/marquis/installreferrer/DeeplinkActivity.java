62a4b4625b07ee8883e91aadccb3d70029e1c632
mllu <mornlyn@gmail.com>
2017-11-18 00:32:35 -0800

    separate deeplink scheme for activity main and deeplink

--- app/src/main/java/fr/simon/marquis/installreferrer/DeeplinkActivity.java
+++ app/src/main/java/fr/simon/marquis/installreferrer/DeeplinkActivity.java
@@ -2,8 +2,11 @@ package fr.simon.marquis.installreferrer;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -16,19 +19,36 @@ public class DeeplinkActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deeplink);
 
+        deeplink = (TextView) findViewById(R.id.deeplink);
+        installReferrer = (TextView) findViewById(R.id.install_referrer);
+
+
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
+        Uri data = intent.getData();
+        if (data != null && data.isHierarchical()) {
+
+            String uri = this.getIntent().getDataString();
+//            Log.d("debug", "deeplink: "+data.toString());
+            Log.d("debug", "Deep link clicked " + uri);
+            StringBuilder sb = new StringBuilder();
+            sb.append("<b>Deeplink:</b>")
+                    .append("<br/>")
+                    .append(uri);
+            deeplink.setText(Html.fromHtml(sb.toString()));
+            deeplink.setMovementMethod(new LinkMovementMethod());
+            return;
+        }
+
         String dl = intent.getStringExtra(MainActivity.DEEPLINK);
         Log.d("debug", "got deeplink " + dl);
 
         // Capture the layout's TextView and set the string as its text
-        deeplink = (TextView) findViewById(R.id.deeplink);
         deeplink.setText(dl);
 
         String ir = intent.getStringExtra(MainActivity.INSTALL_REFERRER);
         Log.d("debug", "got install referrer " + ir);
 
-        installReferrer = (TextView) findViewById(R.id.install_referrer);
         installReferrer.setText(ir);
     }
 }
